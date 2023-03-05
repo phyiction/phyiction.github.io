9f77a69fc4ae3f501738e2c05c10f5ac8cea0a18
phyiction <>
2023-03-04 18:49:04 -0600

    Update assess app from 0.0.1 to 0.0.2

---
+++ static/projects/assess-app/789.bundle.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[789],{789:s=>{s.exports=JSON.parse('{"assessments":[{"id":1,"name":"What\'s my Temperament?","assessment":"temperaments.json"},{"id":2,"name":"Spiritual Gifts Survey","assessment":"spiritual_gifts.json"}]}')}}]);
